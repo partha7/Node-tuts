c7aae9c216e701c6f1d1469ed6da124cbc1be285
partha7 <parthashum@gmail.com>
2021-06-10 22:50:32 +0530

    initial commit

---
+++ myfirstapp.js
@@ -0,0 +1,19 @@
+
+//My First Node app
+
+// 1.
+// const number = 100;
+
+// if(number > 10){
+//     console.log(`It is a large number ${number}`);
+// }
+// else{
+//     console.log(`It is a small number ${number}`);
+// }
+
+// 2. Globl object in Node JS
+
+// console.log(module);
+// console.log(process);
+// console.log(__dirname);
+
