9dacbecf6ec6931591c6752335e873dacfc38e95
partha7 <parthashum@gmail.com>
2021-06-25 13:18:51 +0530

    added comments

--- nodejs_concepts/BuiltInModules/4-fsModuleSync.js
+++ nodejs_concepts/BuiltInModules/4-fsModuleSync.js
@@ -7,3 +7,9 @@ const second = readFileSync('./pathExample/fs2.txt', 'utf-8');
 //writeFileSync will create a file if its not already present
 writeFileSync("./pathExample/theResultofFS1andFS2.txt",
  `Here is the result : ${first} ${second}`, {flag: 'a'});
+
+ // Following are the two ways of appending into a file
+
+// fs.writeFileSync("notes.txt", "Tum kon?", {flag : 'a'})
+
+// fs.appendFileSync("notes.txt", " Hum hai shahenshah, naam to suna hi hoga")
--- nodejs_concepts/Modules/invoke.js
+++ nodejs_concepts/Modules/invoke.js
@@ -1,3 +1,8 @@
+// all the files in node js has their individual scope. 
+// So using require we can run another file, eg: log statements or even function invokes
+// but we cannot reference to any members of the other file from some other file.
+// To access we will need to export.
+
 const names = require('./names');
 const sayHi = require('./function')
 require('./noexport')
